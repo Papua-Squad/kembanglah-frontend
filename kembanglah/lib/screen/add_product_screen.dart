3cce34c59836a3f4fbc3ecf9746ea1588da6bf48
Dimas bayu <dimas31bayu@gmail.com>
2021-12-13 19:28:07 +0700

    produk

--- kembanglah/lib/screen/add_product_screen.dart
+++ kembanglah/lib/screen/add_product_screen.dart
@@ -1,9 +1,23 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:kembanglah/api/url_api.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-class AddProductScreen extends StatelessWidget {
+class AddProductScreen extends StatefulWidget {
+  @override
+  _AddProductScreenState createState() => _AddProductScreenState();
+}
+
+class _AddProductScreenState extends State<AddProductScreen> {
+  File? _imageFile;
+  final ImagePicker _picker = ImagePicker();
+
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final Controller1 = TextEditingController();
   final Controller2 = TextEditingController();
@@ -11,7 +25,68 @@ class AddProductScreen extends StatelessWidget {
   final Controller4 = TextEditingController();
   final Controller5 = TextEditingController();
   final Controller6 = TextEditingController();
-  
+  final storage = FlutterSecureStorage();
+
+  String? selectedValue;
+  List categoryItemList = [];
+
+  Future? getAllCategory() async {
+    var response = await http.get(Uri.parse(ConstUrl.baseUrl + 'api/product/'));
+    if (response.statusCode == 200) {
+      var jsonData = json.decode(response.body);
+      setState(() {
+        categoryItemList = jsonData;
+      });
+    }
+    print(categoryItemList);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getAllCategory();
+  }
+
+  void _showPicker(context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return SafeArea(
+            child: Container(
+              child: new Wrap(
+                children: <Widget>[
+                  new ListTile(
+                      leading: new Icon(Icons.photo_library),
+                      title: new Text('Photo Library'),
+                      onTap: () {
+                        _getFromGallery();
+                        Navigator.of(context).pop();
+                      }),
+                  new ListTile(
+                    leading: new Icon(Icons.photo_camera),
+                    title: new Text('Camera'),
+                    onTap: () {
+                      _getFromCamera();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+  Future<String?> uploadImage(filename, url) async {
+    var request = http.MultipartRequest(
+        'POST', Uri.parse(ConstUrl.baseUrl + 'api/product/'));
+    request.files.add(await http.MultipartFile.fromPath('picture', filename));
+    var res = await request.send();
+
+    return res.reasonPhrase;
+  }
+
+  String state = "";
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -126,6 +201,21 @@ class AddProductScreen extends StatelessWidget {
                     SizedBox(
                       height: 20,
                     ),
+                    DropdownButton(
+                      hint: Text("Pilih Kategori"),
+                      value: selectedValue,
+                      items: categoryItemList.map((categoryItemList) {
+                        return DropdownMenuItem(
+                          child: Text(categoryItemList),
+                          value: categoryItemList,
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          selectedValue = value.toString();
+                        });
+                      },
+                    ),
                     TextFormField(
                       controller: Controller3,
                       decoration: InputDecoration(
@@ -172,83 +262,147 @@ class AddProductScreen extends StatelessWidget {
                       },
                     ),
                     SizedBox(
-                      height: 20,
+                      height: 30,
+                    ),
+                    Container(
+                      child: _imageFile == null
+                          ? Container(
+                              alignment: Alignment.center,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      primary: Colors.white,
+                                      side: BorderSide(
+                                          color: Color(0xff00A38C),
+                                          width: 2.0,
+                                          style: BorderStyle.solid),
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(5.0)),
+                                      minimumSize: Size(370,
+                                          60), // double.infinity is the width and 30 is the height
+                                    ),
+                                    onPressed: () {
+                                      _showPicker(context);
+                                    },
+                                    child: Text(
+                                      'Upload Produk',
+                                      style: TextStyle(
+                                        color: Color(0xff00A38C),
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    height: 40.0,
+                                  ),
+                                  // ElevatedButton(
+                                  //   style: ElevatedButton.styleFrom(
+                                  //     primary: Colors.white,
+                                  //     side: BorderSide(
+                                  //         color: Color(0xff00A38C),
+                                  //         width: 2.0,
+                                  //         style: BorderStyle.solid),
+                                  //     shape: RoundedRectangleBorder(
+                                  //         borderRadius:
+                                  //             BorderRadius.circular(5.0)),
+                                  //     minimumSize: Size(370,
+                                  //         60), // double.infinity is the width and 30 is the height
+                                  //   ),
+                                  //   onPressed: () {
+                                  //     _getFromCamera();
+                                  //   },
+                                  //   child: Text(
+                                  //     'Upload Produk',
+                                  //     style: TextStyle(
+                                  //       color: Color(0xff00A38C),
+                                  //     ),
+                                  //   ),
+                                  // ),
+                                ],
+                              ),
+                            )
+                          : Container(
+                              child: Image.file(
+                                _imageFile!,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
                     ),
                     ElevatedButton(
                         style: ElevatedButton.styleFrom(
                           primary: Color(0xff00A38C),
-                          minimumSize: Size(350, 40),
+                          minimumSize: Size(370, 40),
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(
                                   5.0)), // double.infinity is the width and 30 is the height
                         ),
-                        onPressed: () {},
+                        onPressed: () async {
+                          // var token = await storage.read(key: "Token");
+                          // Response _response = await post(
+                          //   Uri.parse(ConstUrl.baseUrl + 'api/product/'),
+                          //   headers: {
+                          //     'Content-Type': 'application/json',
+                          //     'Accept': 'application/json',
+                          //     'Authorization': 'Bearer $token',
+                          //   },
+                          //   body: <String, dynamic>{
+                          //     'name': Controller1.text,
+                          //     'price': Controller2.text,
+                          //     'stock': Controller3.text,
+                          //     'weight': Controller4.text,
+                          //     'description': Controller5.text,
+                          //     'category_id': Controller6.text,
+                          //     'image': 'test',
+                          //   },
+                          // );
+                          // print(_response.body);
+                          // if (_formKey.currentState!.validate() &&
+                          //     _response.statusCode == 200) {
+                          //   Navigator.pop(context);
+                          // } else {
+                          //   showDialog<String>(
+                          //     context: context,
+                          //     builder: (BuildContext context) => AlertDialog(
+                          //       title: const Text(
+                          //           'Pemberitahuan Username/Password salah'),
+                          //       content: const Text(
+                          //           'Apakah Username & Password anda masukan benar?'),
+                          //       actions: <Widget>[
+                          //         TextButton(
+                          //           onPressed: () => Navigator.pop(context),
+                          //           child: const Text('OK'),
+                          //         ),
+                          //       ],
+                          //     ),
+                          //   );
+                          // }
+                        },
                         child: Text('Tambah Produk')),
                     SizedBox(
                       height: 15,
                     ),
                     ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.white,
-                          side: BorderSide(
-                              color: Color(0xff00A38C),
-                              width: 2.0,
-                              style: BorderStyle.solid),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(5.0)),
-                          minimumSize: Size(350,
-                              40), // double.infinity is the width and 30 is the height
-                        ),
-                        onPressed: () async {
-                          // validate form
-
-                          Response _response = await post(
-                            Uri.parse(ConstUrl.baseUrl + 'api/product/'),
-                            headers: {
-                              'Content-Type': 'application/json',
-                              'Accept': 'application/json',
-                              'Authorization': 'Bearer $token',
-                            },
-                            body: <String, dynamic>{
-                              'name': Controller1.text,
-                              'price': Controller2.text,
-                              'stock': Controller3.text,
-                              'weight': Controller4.text,
-                              'description': Controller5.text,
-                              'category_id': Controller6.text,
-                              'image': 'test',
-                            },
-                          );
-                          print(_response.body);
-                          if (_formKey.currentState!.validate() &&
-                              _response.statusCode == 200) {
-                            Navigator.pop(context);
-                          } else {
-                            showDialog<String>(
-                              context: context,
-                              builder: (BuildContext context) => AlertDialog(
-                                title: const Text(
-                                    'Pemberitahuan Username/Password salah'),
-                                content: const Text(
-                                    'Apakah Username & Password anda masukan benar?'),
-                                actions: <Widget>[
-                                  TextButton(
-                                    onPressed: () => Navigator.pop(context),
-                                    child: const Text('OK'),
-                                  ),
-                                ],
-                              ),
-                            );
-                          }
-
-                          // get provider read
-                        },
-                        child: Text(
-                          'Edit Produk',
-                          style: TextStyle(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.white,
+                        side: BorderSide(
                             color: Color(0xff00A38C),
-                          ),
-                        ))
+                            width: 2.0,
+                            style: BorderStyle.solid),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5.0)),
+                        minimumSize: Size(370,
+                            40), // double.infinity is the width and 30 is the height
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'Edit Produk',
+                        style: TextStyle(
+                          color: Color(0xff00A38C),
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
@@ -256,4 +410,37 @@ class AddProductScreen extends StatelessWidget {
           ),
         ));
   }
+
+  _getFromGallery() async {
+    _imageFile = File(await ImagePicker()
+        .pickImage(source: ImageSource.gallery)
+        .then((pickedFile) => pickedFile!.path));
+    print(_imageFile);
+  }
+
+  /// Get from Camera
+  _getFromCamera() async {
+    _imageFile = File(await ImagePicker()
+        .pickImage(source: ImageSource.camera)
+        .then((pickedFile) => pickedFile!.path));
+  }
+}
+
+uploadFile(String name, String price, String stock, String weight,
+    String description, String category_id) async {
+  var postUri = Uri.parse(ConstUrl.baseUrl + 'api/product/');
+  var request = new http.MultipartRequest("POST", postUri);
+  request.fields['name'] = name;
+  request.fields['price'] = price;
+  request.fields['stock'] = stock;
+  request.fields['weight'] = weight;
+  request.fields['description'] = description;
+  request.fields['category_id'] = category_id;
+  request.files.add(new http.MultipartFile.fromBytes(
+      'image', await File.fromUri(postUri).readAsBytes(),
+      contentType: new MediaType('image', 'jpeg')));
+
+  request.send().then((response) {
+    if (response.statusCode == 200) print("Uploaded!");
+  });
 }
